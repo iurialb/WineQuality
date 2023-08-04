21d1dc6822f977bc959ff63e3eb652c1eb864131
iurialb <iurialbuquerque@protonmail.com>
2023-08-04 09:31:40 -0300

    update

--- Sem Título.R
+++ Sem Título.R
@@ -1,2 +1,12 @@
 
+# Tornando os resultados reproduzíveis
 set.seed(0)
+
+# Lendo a nossa base de dados
+
+data <- read.csv(file = "winequality-red.csv")
+
+# Alterando a escala dos dados para melhor processamento
+# Isso porque redes neurais são menos performáticas com altos valores
+
+data <- scale(data)

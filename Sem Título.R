b8dd740ffdef1294f4c9a00f512d2e5b4e88a061
iurialb <iurialbuquerque@protonmail.com>
2023-08-04 11:03:24 -0300

    finalizacao do modelo

--- Sem Título.R
+++ Sem Título.R
@@ -121,7 +121,6 @@ fwd_prop <- forwardPropagation(train_x, init_params, layer_size)
 # Isso ocorre, porque utilizamos valores aleatórios nos pesos para saber o quanto estamos errando
 # Nesse sentido, vamos agora utilizar a função custo para realizar o processo de Back propagation com o Erro Quadrático Médio
 
-
 computeCost <- function(y, cache) {
   
   m <- dim(y)[2]
@@ -133,18 +132,109 @@ computeCost <- function(y, cache) {
   return (cost)
 }
 
+cost <- computeCost(train_y, fwd_prop)
 
+# Função de Back Propagation
 
+backwardPropagation <- function(X, y, cache, params, layer_size){
+  
+  m <- dim(X)[2]
+  
+  n_x <- layer_size$n_x
+  n_h <- layer_size$n_h
+  n_y <- layer_size$n_y
+  
+  A2 <- cache$A2
+  A1 <- cache$A1
+  W2 <- params$W2
+  
+  
+  dZ2 <- A2 - y
+  dW2 <- 1/m * (dZ2 %*% t(A1)) 
+  
+  
+  dZ1 <- (t(W2) %*% dZ2) * (1 - A1^2)
+  dW1 <- 1/m * (dZ1 %*% t(X))
+  
+  
+  grads <- list("dW1" = dW1, 
+                "dW2" = dW2)
+  
+  return(grads)
+}
 
+# Atualização dos pesos
+# A atualização de pesos é feita com base nos cálculos anteriores e na taxa de aprendizagem.
 
+updateParameters <- function(grads, params, learning_rate){
+  
+  W1 <- params$W1
+  W2 <- params$W2
+  
+  dW1 <- grads$dW1
+  dW2 <- grads$dW2
+  
+  
+  W1 <- W1 - learning_rate * dW1
+  W2 <- W2 - learning_rate * dW2
+  
+  updated_params <- list("W1" = W1,
+                         "W2" = W2)
+  
+  return (updated_params)
+}
 
+# Treina o modelo
+## Quais as etapas?
+# - Arquitetura da rede.
+# - Inicializa um vetor de historia da função custo para guardar resultados.
+# - Faz foward loop.
+# - Calcula perda.
+# - Atualiza parâmetros.
+# - Usa os novos parâmetros.
 
+# Entra o conceito de épocas (EPOCHS)
+# Basicamente diz respeito ao modelo fazer um "vai e volta" para medida de treinamento até achar o valor mais próximo do real
 
+trainModel <- function(X, y, num_iteration, hidden_neurons, lr){
+  
+  layer_size <- getLayerSize(X, y, hidden_neurons)
+  init_params <- initializeParameters(X, layer_size)
+  
+  cost_history <- c()
+  
+  for (i in 1:num_iteration) {
+    fwd_prop <- forwardPropagation(X, init_params, layer_size)
+    cost <- computeCost(y, fwd_prop)
+    back_prop <- backwardPropagation(X, y, fwd_prop, init_params, layer_size)
+    update_params <- updateParameters(back_prop, init_params, learning_rate = lr)
+    init_params <- update_params
+    cost_history <- c(cost_history, cost)
+    
+  }
+  
+  model_out <- list("updated_params" = update_params,
+                    "cost_hist" = cost_history)
+  
+  return (model_out)
+}
 
+# Aplica o treinamento
 
+EPOCHS = 100
+HIDDEN_NEURONS = 40
+LEARNING_RATE = 0.01
 
+train_model <- trainModel(train_x, train_y, hidden_neurons = HIDDEN_NEURONS, num_iteration = EPOCHS, lr = LEARNING_RATE)
 
+# Testa os resultados
 
+layer_size <- getLayerSize(test_x, test_y, HIDDEN_NEURONS)
+params <- train_model$updated_params
+fwd_prop <- forwardPropagation(test_x, params, layer_size)
+y_pred <- fwd_prop$A2
+compare <- rbind(y_pred,test_y)
 
+# Verifica a função custo
 
-
+plot(train_model$cost_hist)
